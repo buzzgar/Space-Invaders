ab312b856ee8141d89be562a877235f9108a8582
buzzgar <87481868+buzzgar@users.noreply.github.com>
2025-04-15 22:25:11 +0200

    Update background.py

--- utils/background.py
+++ utils/background.py
@@ -4,4 +4,9 @@ import GameSettings
 
 class Background:
   def __init__(self):
-    self.background = Picture.GameSettings.background
+    self.background = Picture(GameSettings.background_sprite_path)
+    self.w = self.background.width()
+    self.h = self.background.height() 
+
+def display(self): 
+  stddraw.picture(self.background, self.w / 2, self.h / 2) 
