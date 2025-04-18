d019c05ec0e26fd8c58b568a3cb6b46977dd7c1a
buzzgar <87481868+buzzgar@users.noreply.github.com>
2025-04-18 04:52:41 +0200

    Update GameOver.py

--- GameOver.py
+++ GameOver.py
@@ -1,5 +1,30 @@
 import stddraw
+from menu import Gif
 
+class Gif:
+    def __init__(self, name, num_frames):
+        self.frames = stdarray.create1D(num_frames, "")  # create array to store each frame
+        self.name = name
+        for i in range(0, num_frames):  # populate array
+            self.frames[i] = Picture("assets/" + self.name + "_" + str(i + 1) + ".png")
+
+        self.dimensions = self.frames[0]  # set up standard dimensions for all frames
+
+        self.w = self.dimensions.width()
+        self.h = self.dimensions.height()
+
+        self.x_centre = self.w / 2
+        self.y_centre = self.h / 2
+
+    def setup(self):
+        stddraw.setCanvasSize(self.w, self.h)  # use dimensions of image as canvas size
+
+        stddraw.setXscale(0.0, self.w)  # sets canvas to image dimensions
+        stddraw.setYscale(0.0, self.h)
+
+    def draw_frame(self, i):
+        stddraw.clear()
+        stddraw.picture(self.frames[i], self.x_centre, self.y_centre)
 
 class GameOverScreen:
     def __init__(self, width, height):
@@ -8,15 +33,6 @@ class GameOverScreen:
 
         self.x_centre = self.w // 2
         self.y_centre = self.h // 2
-
-    def game_over(self):
-
-        stddraw.clear(stddraw.RED)
-
-        stddraw.setPenColor(stddraw.WHITE)
-        stddraw.setFontSize(50)
-        stddraw.text(self.x_centre, self.y_centre, "Game Over")
-        stddraw.text(self.x_centre, self.y_centre - 100, "[R] to restart")
 
     def success(self):
 
