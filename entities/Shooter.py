822a90e74601309cbf42bd567ce23a0c69b084f9
Samkelo Nkabinde <samkelonkabinde33@gmail.com>
2025-04-21 00:10:12 +0200

    Update Shooter.py
    updated shooter

--- entities/Shooter.py
+++ entities/Shooter.py
@@ -5,11 +5,11 @@ import math
 
 import GameSettings
 from utils.utils import GameObject
-
+from picture import Picture
 
 #assumes a scale of -1,1 for both x and y
 class Shooter(GameObject):
-    def __init__(self, name, x, y, width, height, color, scaleFactor = 0.1, playerFile = 'player1.txt'):
+    def __init__(self, name, x, y, width, height, color, scaleFactor = 0.1, playerFile = 'Player1.png'):
 
         self._shooter = self._getShooterShape(playerFile)
 
@@ -103,38 +103,43 @@ class Shooter(GameObject):
     def _draw(self):
         for i in range(len(self._shooter)):
             for j in range(len(self._shooter[0])):
-                if self._shooter[i][j] != 0:
+                color = self._shooter[i][j]
+                if (color.getRed(),color.getGreen(),color.getBlue()) != (0,0,0): 
                     x = self._x + j * self._pixelWidth
                     y = self._y + (len(self._shooter) - i - 1) * self._pixelHeight
-
+                
                     Xcorners = [x, x + self._pixelWidth, x + self._pixelWidth, x]
                     Ycorners = [y, y, y + self._pixelHeight, y + self._pixelHeight]
 
-                    rotatedXconners = []
-                    rotatedYconners = []
+                    rotatedXcorners = []
+                    rotatedYcorners = []
                     centerX = self._x + self._width / 2
                     centerY = self._y + self._heigth / 2
+
                     for k in range(4):
                         rx, ry = self._rotatePoints(Xcorners[k], Ycorners[k], centerX, centerY, self._angle)
-                        rotatedXconners.append(rx)
-                        rotatedYconners.append(ry)
+                        rotatedXcorners.append(rx)
+                        rotatedYcorners.append(ry)
 
-                    if self._shooter[i][j] == 2:
-                        stddraw.setPenColor(stddraw.RED)
-                    else:
-                        stddraw.setPenColor(stddraw.BOOK_BLUE)
-                    stddraw.filledPolygon(rotatedXconners, rotatedYconners)
+                    stddraw.setPenColor(color)
+                    stddraw.filledPolygon(rotatedXcorners, rotatedYcorners)
+
         
-    def _getShooterShape(self,file_path):
-        shapeMatrix = []
-        with open(file_path, 'r') as file:
-            for line in file:
-                row = [int(x) for x in line.strip().split(',')]
-                shapeMatrix.append(row)
-        return shapeMatrix
+    def _getShooterShape(self,filePath):
+        pic = Picture(filePath)
+        picMatrix = [[None] * pic.width()  for i in range(pic.height())]
+        for i in range(pic.height()):
+            for j in range(pic.width()):
+                color = pic.get(j,i)
+                picMatrix[i][j] =  color
+
+        return picMatrix
+
 
     def drawShooter(self):
         self._draw()
 
+
+    
 
     
